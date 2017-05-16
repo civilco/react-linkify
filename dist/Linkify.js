7000fdfb7afd5d4325dcd9df9623dfc8ef51b820
Asa Miller <asa@Hypnotoad.local>
2017-05-16 13:24:34 -0700

    ditch tlds for good

--- dist/Linkify.js
+++ dist/Linkify.js
@@ -14,10 +14,6 @@ var _linkifyIt = require('linkify-it');
 
 var _linkifyIt2 = _interopRequireDefault(_linkifyIt);
 
-var _tlds = require('tlds');
-
-var _tlds2 = _interopRequireDefault(_tlds);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -26,14 +22,16 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+// import tlds from 'tlds';
+
 var linkify = new _linkifyIt2.default();
-linkify.tlds(_tlds2.default);
+// linkify.tlds(tlds);
 
 var Linkify = function (_React$Component) {
   _inherits(Linkify, _React$Component);
 
   function Linkify() {
-    var _Object$getPrototypeO;
+    var _ref;
 
     var _temp, _this, _ret;
 
@@ -43,7 +41,7 @@ var Linkify = function (_React$Component) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Linkify)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.parseCounter = 0, _temp), _possibleConstructorReturn(_this, _ret);
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Linkify.__proto__ || Object.getPrototypeOf(Linkify)).call.apply(_ref, [this].concat(args))), _this), _this.parseCounter = 0, _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(Linkify, [{
